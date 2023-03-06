418c4354e1fb6134a842b4cb774750cc891c369c
Kourtney Smith <klsmith5370@gmail.com>
2023-03-06 17:28:57 -0500

    Added click handler for logging random meme url

--- components/Meme.js
+++ components/Meme.js
@@ -1,13 +1,21 @@
 import React from "react"
+import memesData from "../memesData"
 
 export default function Meme() {
+    function getMemeImage() { // click event handler for getting a random image
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        const url = memesArray[randomNumber].url
+        // console.log(url)
+    }
+
     return (
         <main>
-            <form className="form">
+            <div className="form">
                 <input type="text" placeholder="Top text" className="form--input"></input>
                 <input type="text" placeholder="Bottom text" className="form--input"></input>
-                <button className="form--button">Get a new meme image</button>
-            </form>
+                <button onClick={getMemeImage} className="form--button">Get a new meme image</button>
+            </div>
         </main>
     )
 }
