31721b506a63557a6ce000e2885a133c834e2bae
Kourtney Smith <klsmith5370@gmail.com>
2023-03-09 11:26:43 -0500

    Added state for generating random meme

--- README.md
+++ README.md
@@ -18,4 +18,3 @@ Run Webpack in watch-mode to continually compile the JavaScript as you work:
 ```
 $ yarn watch # npm run watch
 ```
-
--- components/Meme.js
+++ components/Meme.js
@@ -2,11 +2,12 @@ import React from "react"
 import memesData from "../memesData"
 
 export default function Meme() {
+    const [memeImage, setMemeImage] = React.useState("")
+
     function getMemeImage() { // click event handler for getting a random image
         const memesArray = memesData.data.memes
         const randomNumber = Math.floor(Math.random() * memesArray.length)
-        const url = memesArray[randomNumber].url
-        // console.log(url)
+        setMemeImage(memesArray[randomNumber].url)
     }
 
     return (
@@ -15,6 +16,7 @@ export default function Meme() {
                 <input type="text" placeholder="Top text" className="form--input"></input>
                 <input type="text" placeholder="Bottom text" className="form--input"></input>
                 <button onClick={getMemeImage} className="form--button">Get a new meme image</button>
+                <img src={memeImage} className="meme--image"/>
             </div>
         </main>
     )
